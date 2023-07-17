1a20370edc1fbf2f386bd8e0c44350e4f7080bda
divitoau <divitoau@gmail.com>
2023-07-16 20:18:14 -0400

    created and imported a main view

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,10 @@
+export const MainView = () => {
+  return (
+    <div>
+      <div>Sucker Punch</div>
+      <div>Mad Max</div>
+      <div>Blade Runner</div>
+      <div>Repo!</div>
+    </div>
+  );
+};
