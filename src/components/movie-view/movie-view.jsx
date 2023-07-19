1961110978669c6ce97b1bb45486785740fcd92d
divitoau <divitoau@gmail.com>
2023-07-18 22:52:57 -0400

    updated data to be similar to that contained in mongodb

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,16 +2,23 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
+        <img src={movie.ImagePath} alt="" />
+      </div>
+      <div>
         <span>Title: </span>
-        <span>{movie.title}</span>{" "}
+        <span>{movie.Title}</span>{" "}
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
-      </div>
-      <div>
-        <span>Year: </span>
-        <span>{movie.year}</span>
+        <span>{movie.Director.Name}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
